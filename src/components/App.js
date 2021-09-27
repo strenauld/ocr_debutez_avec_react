9cf8cbb915c2f5f5108fb30c87dc129b5bc58077
Stéphane RENAULD <stephane.renauld@kaizen-solutions.net>
2021-09-27 16:23:29 +0200

    prenez en main create react app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner';
+import Cart from './Cart';
+
+import '../App.css';
+
+function App() {
+  return (
+  <div>
+    <Banner/>
+    <Cart/>
+  </div>)
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,6 @@
+function Banner() {
+    const bannerText = "La maison jungle";
+    return <h1>{ bannerText }</h1>
+}
+
+export default Banner;
