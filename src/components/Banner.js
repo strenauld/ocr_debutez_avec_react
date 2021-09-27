d7ec7edde1e663990b1e77137395e7b68e791a7f
Stéphane RENAULD <stephane.renauld@kaizen-solutions.net>
2021-09-27 17:05:40 +0200

    incorporez du style et des assets a votre projet

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,14 @@
+import '../styles/Banner.css';
+import logo from '../assets/logo.png';
+
 function Banner() {
     const bannerText = "La maison jungle";
-    return <h1>{ bannerText }</h1>
+    return (
+        <div className="lmj-banner">
+            <img src={ logo } alt="La maison jungle" className="lmj-logo"/>
+            <h1 className="lmj-title">{ bannerText }</h1>
+        </div>
+    )
 }
 
 export default Banner;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,17 @@
+import '../styles/Cart.css'
+
 function Cart() {
     const monsteraPrice = 8;
     const ivyPrice = 10;
     const flowerPrice = 15;
     
-    return  (
-        <div>
+    return (
+        <div className="lmj-cart">
             <h2>Panier</h2>
             <ul>
-                <li>Monstera: { monsteraPrice }</li>
-                <li>Lierre: { ivyPrice }</li>
-                <li>Fleurs: { flowerPrice }</li>
+                <li>Monstera: { monsteraPrice }€</li>
+                <li>Lierre: { ivyPrice }€</li>
+                <li>Fleurs: { flowerPrice }€</li>
             </ul>
             Total: { monsteraPrice + ivyPrice + flowerPrice }€
         </div>
