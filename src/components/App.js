8b9dd576af53afc384ed18158eaa10f55a89db2d
strenauld <stephane.renauld1@gmail.com>
2021-09-30 17:26:39 +0200

    declenchez des effets avec useeffect -2-

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,14 @@ import Footer from './Footer';
 import logo from '../assets/logo.png';
 
 import '../App.css';
-import { useState } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react/cjs/react.development';
 
 function App() {
-  const [cart, updateCart] = useState([]);
+  const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
 
   return (
     <div>
